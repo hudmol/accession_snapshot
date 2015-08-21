e82f56bda2070df16adf88480ced7ddc82d6cc62
Payten Giles <payten.giles@gmail.com>
2015-08-21 14:10:36 +1000

    Force snapshot to deliver as a download with a useful filename. Also resolve a few extra linked records (payment authorizer and top_container's container_profile).

--- frontend/controllers/accession_snapshot_controller.rb
+++ frontend/controllers/accession_snapshot_controller.rb
@@ -12,6 +12,31 @@ class AccessionSnapshotController < ApplicationController
       end
     end
 
-    render :json => accession.to_json
+    ##Render json inline (for testing)
+    #render :json => accession.to_json
+    deliver_as_download(accession)
+  end
+
+
+  private
+
+  def deliver_as_download(accession)
+    send_data accession.to_json,
+              :filename => generate_filename(accession),
+              :type => 'application/json',
+              :disposition => 'attachment'
+  end
+
+  def generate_filename(accession)
+    now = Time.now.strftime("%Y%m%d%H%M%S")
+    "repositories_#{session[:repo_id]}_accessions_#{params[:id]}.#{now}.json"
+  end
+
+  def find_opts
+    opts = super
+    opts["resolve[]"] << "authorizer"
+    opts["resolve[]"] << "top_container::container_profile"
+
+    opts
   end
 end
